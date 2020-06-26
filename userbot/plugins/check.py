16b3545e3ec0580d0a32fb868043723c0fe4b056
Kazutettoh <66389612+Kazutettoh@users.noreply.github.com>
2020-06-26 20:02:15 +0200

    Update check.py

--- userbot/plugins/check.py
+++ userbot/plugins/check.py
@@ -1,3 +1,13 @@
+import asyncio
+import random
+import logging
+
+from collections import deque
+from telethon import events
+from telethon.errors.rpcerrorlist import MessageIdInvalidError
+from userbot import bot
+from userbot.system import dev_cmd
+
 @bot.on(dev_cmd(pattern=f"check", outgoing=True))
 async def _(event):
     if event.fwd_from:
