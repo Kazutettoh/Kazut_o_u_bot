ed250351ebc49990e0f57d0e89730ce8decc6051
Kazutettoh <66389612+Kazutettoh@users.noreply.github.com>
2020-06-26 20:34:12 +0200

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,19 +1,4 @@
-# Copyright © 2020 di 100101110 Github, <https://github.com/100101110>.
-#
-# Questo file fa parte del progetto <https://github.com/100101110/userbot-100101110>,
-# e viene rilasciato in base alla "Licenza GNU Affero General Public v3.0".
-# Si prega di consultare <https://github.com/100101110/userbot-100101110/blob/master/LICENSE>
-#
-# Tutti i diritti riservati.
-  
-import asyncio
-from userbot import ALIVE_NAME, bot, CMD_LIST
-from userbot.system import command
-from platform import uname
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
-# ============================================
+from userbot import CMD_LIST
 
 @command(pattern="^.help ?(.*)")
 async def cmd_list(event):
@@ -29,22 +14,22 @@ async def cmd_list(event):
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                await bot.send_message(event.chat_id, "Do .help cmd")
+                await borg.send_message(event.chat_id, "Do .help cmd")
                 await asyncio.sleep(5)
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Comandi in {}:\n".format(input_str)
+                string = "Commands found in {}:\n".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
                 await event.edit(string)
             else:
-                await event.edit(input_str + " plugin non valido!")
+                await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""Userbot Helper.. di {DEFAULTUSER}\n\n
-`Helper per i comandi`\nPremi .help plugin_name se non è qui."""
+            help_string = """Userbot Helper di {DEFAULTUSER} \n\n
+`Userbot Helper per i comandi`\n__Premi .help plugin_name, in caso non sia qui.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
